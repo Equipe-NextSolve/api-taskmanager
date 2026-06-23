39a6168a41e87d8493aa296c8eca5a98c7edef4b
Guilherme <guilhermebarroso166@gmail.com>
2026-06-23 15:47:41 -0300

    ajuste nos planos

--- src/constants/plans.ts
+++ src/constants/plans.ts
@@ -1,13 +1,13 @@
-// src/constants/plans.ts
+
 export const PLANS = {
-  BASIC: {
-    name: 'BASIC',
-    price: 29.9,
-    days: 30
-  },
-  PRO: {
-    name: 'PRO',
-    price: 99.9,
-    days: 30
-  }
+    BASIC: {
+        name: 'BASIC',
+        price: 29.9,
+        days: 30
+    },
+    PRO: {
+        name: 'PRO',
+        price: 99.9,
+        days: 30
+    }
 };
